58010091003b571160b45ff1b96c2f40442a79db
gargi <gargimoghe24@gmail.com>
2025-04-11 14:33:17 +0530

    added hexadecimal calculations

--- cal.c
+++ cal.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
-#include <math.h>  // Added
+#include <math.h>
+#include <string.h>
+
+int isHex(char *s) {
+    return (strlen(s) > 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X'));
+}
 
 int main() {
+    char strA[20], strB[20];
     double a, b;
     char op;
 
-    printf("Enter first number: ");
-    scanf("%lf", &a);
+    printf("Enter first number (prefix 0x for hex): ");
+    scanf("%s", strA);
 
-    printf("Enter operator (+, -, *, /, ^): ");  // Updated prompt
+    printf("Enter operator (+, -, *, /, ^): ");
     scanf(" %c", &op);
 
-    printf("Enter second number: ");
-    scanf("%lf", &b);
+    printf("Enter second number (prefix 0x for hex): ");
+    scanf("%s", strB);
+
+    // Convert inputs
+    if (isHex(strA))
+        sscanf(strA, "%x", (int *)&a);
+    else
+        sscanf(strA, "%lf", &a);
+
+    if (isHex(strB))
+        sscanf(strB, "%x", (int *)&b);
+    else
+        sscanf(strB, "%lf", &b);
 
     switch(op) {
         case '+': printf("Result: %.2lf\n", a + b); break;
@@ -22,7 +39,7 @@ int main() {
             if (b != 0) printf("Result: %.2lf\n", a / b);
             else printf("Error: Division by zero\n");
             break;
-        case '^': printf("Result: %.2lf\n", pow(a, b)); break;  // New case
+        case '^': printf("Result: %.2lf\n", pow(a, b)); break;
         default: printf("Invalid operator\n");
     }
 
